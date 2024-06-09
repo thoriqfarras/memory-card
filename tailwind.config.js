39c9e198c967036eb24c5c119b93f7a979a7d1be
thoriq farras <thoriqibrahimfarras@gmail.com>
2024-06-09 09:47:04 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      pixel: ['RETROTECH', 'ui-monospace'],
+    },
+  },
+  plugins: [],
+};
