09b889d00cfb61715d146eb411578fb09da78dd1
thoriq farras <thoriqibrahimfarras@gmail.com>
2025-01-14 17:08:37 +0700

    fixed shuffling logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
+import { fetchPokemons } from './api';
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetchPokemons().then((pokemons) => {
+      setPokemons(pokemons);
+    });
+  }, []);
 
   return (
     <div className="bg-green-900 h-screen w-screen flex flex-col font-pixel text-zinc-100">
@@ -12,16 +20,33 @@ function App() {
         <p>Score: {score}</p>
         <p>Best: {bestScore}</p>
       </div>
-      <CardGrid />
+      <CardGrid
+        pokemons={pokemons}
+        setScore={setScore}
+        setPokemons={setPokemons}
+      />
     </div>
   );
 }
 
-function CardGrid() {
-  const cards = [];
-  for (let i = 0; i < 20; i += 1) {
-    cards.push(<Card />);
-  }
+function CardGrid({ pokemons, setScore, setPokemons }) {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(
+      pokemons.map((pokemon) => (
+        <Card
+          key={pokemon.name}
+          pokemonName={pokemon.name}
+          pokemonSprite={pokemon.img}
+          setScore={setScore}
+          pokemons={pokemons}
+          setPokemons={setPokemons}
+        />
+      ))
+    );
+    console.log('Grid rendering');
+  }, [pokemons]);
 
   return (
     <div className="grid grid-cols-[repeat(auto-fit,_minmax(240px,_1fr))] p-20 gap-8">
@@ -30,15 +55,39 @@ function CardGrid() {
   );
 }
 
-function Card() {
+function Card({ pokemonName, pokemonSprite, setScore, setPokemons, pokemons }) {
+  function shuffle(array) {
+    array = [...array];
+    let m = array.length;
+    let t = 0;
+    let i = 0;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    console.log('array is shuffled');
+    return array;
+  }
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2 hover:cursor-pointer"
+      onClick={() => {
+        setScore((score) => score + 1);
+        const shuffled = shuffle(pokemons);
+        setPokemons(shuffled);
+      }}
+    >
       <img
-        src="/"
-        alt="Pokemon image"
-        className="border-2 border-black rounded-md h-10"
+        src={pokemonSprite}
+        alt={`image of ${pokemonName}`}
+        className="border-2 border-black rounded-md h-20 w-20"
       />
-      <p className="self-center">Pokemon Name</p>
+      <p className="self-center">{pokemonName}</p>
     </div>
   );
 }
